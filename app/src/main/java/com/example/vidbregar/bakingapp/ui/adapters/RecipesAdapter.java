eaabd572dd35b9e1952e0118645b69a6f70df500
Vid Bregar <vidbregar@Vids-iMac.local>
2018-04-24 20:00:28 +0200

    Add minimum viable MainFragment with a list of card view recipes

---
+++ app/src/main/java/com/example/vidbregar/bakingapp/ui/adapters/RecipesAdapter.java
@@ -0,0 +1,57 @@
+package com.example.vidbregar.bakingapp.ui.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.vidbregar.bakingapp.R;
+import com.example.vidbregar.bakingapp.models.Recipe;
+
+import java.util.List;
+
+public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHolder> {
+
+    List<Recipe> recipes;
+
+    // List of recipes must be provided through setRecipes method
+    public RecipesAdapter() { }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_item, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        Recipe recipe = recipes.get(position);
+        viewHolder.recipeTitleTextView.setText(recipe.getName());
+        viewHolder.servingsNumberTextView.setText(Integer.toString(recipe.getServings()));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (recipes == null) return 0;
+        else return recipes.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView recipeTitleTextView;
+        public TextView servingsNumberTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            recipeTitleTextView = itemView.findViewById(R.id.recipe_title_tv);
+            servingsNumberTextView = itemView.findViewById(R.id.servings_number_tv);
+        }
+    }
+
+    public void setRecipes(@NonNull List<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
--- app/src/main/java/com/example/vidbregar/bakingapp/ui/fragments/MainFragment.java
+++ app/src/main/java/com/example/vidbregar/bakingapp/ui/fragments/MainFragment.java
@@ -1,6 +1,42 @@
 package com.example.vidbregar.bakingapp.ui.fragments;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainFragement extends Fragment {
+import com.example.vidbregar.bakingapp.R;
+import com.example.vidbregar.bakingapp.ui.adapters.RecipesAdapter;
+
+public class MainFragment extends Fragment {
+
+    private Context context;
+
+    private RecyclerView recipesRecyclerView;
+    private RecipesAdapter recipesAdapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        context = rootView.getContext();
+
+        recipesRecyclerView = rootView.findViewById(R.id.recipes_recycler_view);
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(context);
+        recipesRecyclerView.setLayoutManager(linearLayoutManager);
+        recipesAdapter = new RecipesAdapter();
+        recipesRecyclerView.setAdapter(recipesAdapter);
+        return rootView;
+    }
 }
