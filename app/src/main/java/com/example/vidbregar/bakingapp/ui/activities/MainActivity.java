de61f633683189788bf09d5d7f7eb00345c6b90e
Vid Bregar <vidbregar@Vids-iMac.local>
2018-04-24 18:46:31 +0200

    Add custom colors and general project structure

--- app/src/main/java/com/example/vidbregar/bakingapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/vidbregar/bakingapp/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.vidbregar.bakingapp;
+package com.example.vidbregar.bakingapp.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.vidbregar.bakingapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
